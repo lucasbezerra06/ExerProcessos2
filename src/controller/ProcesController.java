c7b968d7c29be62e323698b61eb5caedd553a279
lucas <lucas@lucasPC>
2017-08-21 15:39:44 -0300

    Metodos otimizados para linux

--- src/controller/ProcesController.java
+++ src/controller/ProcesController.java
@@ -16,8 +16,16 @@ public class ProcesController {
 	}
 	
 	public void listProcess(String so){
+		String comando = "";
+		
+		if(so.contains("Windows")){
+			comando = "tasklist";
+		}
+		else if(so.contains("Linux")){
+			comando="ps aux";
+		}
 		try {
-			Process p = Runtime.getRuntime().exec("tasklist");
+			Process p = Runtime.getRuntime().exec(comando);
 			InputStream fluxo = p.getInputStream();
 			InputStreamReader lerFluxo= new InputStreamReader(fluxo);
 			BufferedReader buffer = new BufferedReader(lerFluxo);
@@ -33,7 +41,14 @@ public class ProcesController {
 	}
 	
 	public void killProcessPid(String OS,int PID){
-		String cmdPid = "TASKKILL /PID ";
+		String cmdPid = "";
+		if(OS.contains("Windows")){
+			cmdPid = "TASKKILL /PID ";
+		}
+		else if(OS.contains("Linux")){
+			cmdPid = "kill -9 ";
+		}
+		
 		StringBuffer buffer = new StringBuffer();
 			buffer.append(cmdPid);
 			buffer.append(PID);
@@ -46,15 +61,40 @@ public class ProcesController {
 	}
 	
 	public void killProcessName(String OS,String Nome){
-		String cmdNome = "TASKKILL /IM ";
-		StringBuffer buffer = new StringBuffer();
-			buffer.append(cmdNome);
-			buffer.append(Nome);
+		String cmdNome;
+		if(OS.contains("Windows")){
+			cmdNome = "TASKKILL /IM ";
+			StringBuffer buffer = new StringBuffer();
+				buffer.append(cmdNome);
+				buffer.append(Nome);
 		
-		try {
-			Runtime.getRuntime().exec(buffer.toString());
-		} catch (IOException e) {
-			e.printStackTrace();
+			try {
+				Runtime.getRuntime().exec(buffer.toString());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		else if(OS.contains("Linux")){
+			
+			try {
+				int pid;
+				
+				Process p = Runtime.getRuntime().exec("pidof "+Nome);
+				InputStream fluxo = p.getInputStream();
+				InputStreamReader lerFluxo= new InputStreamReader(fluxo);
+				BufferedReader buffer = new BufferedReader(lerFluxo);
+				String linha = buffer.readLine();
+				Nome = linha.substring(0, linha.length());
+				cmdNome = "kill -9 ";
+				StringBuffer stringbuffer = new StringBuffer();
+				stringbuffer.append(cmdNome);
+				stringbuffer.append(Nome);
+				Runtime.getRuntime().exec(stringbuffer.toString());
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 	
--- src/view/Main.java
+++ src/view/Main.java
@@ -5,14 +5,15 @@ import controller.ProcesController;
 public class Main {
 	public static void main(String args[]){
 		int pid;
-		String nome;
+		String nome,osName;
 		ProcesController procesController = new ProcesController();
+		osName = procesController.getOS();
 		procesController.listProcess(procesController.getOS());
 		pid = Integer.parseInt(JOptionPane.showInputDialog("Insira o PID do processo a ser encerrado"));
 		procesController.killProcessPid(procesController.getOS(), pid);
 		nome = JOptionPane.showInputDialog("Insira o nome do processo a ser encerrado");
 		procesController.killProcessName(procesController.getOS(), nome);
-
+		
 	
 	}
 
